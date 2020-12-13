ab29dddc1c13ea67f44884ab7843ad4387f76a6e
Benedito Alberto Macedo <benedito.macedo@gmail.com>
2020-12-12 23:38:32 -0300

    Navigational Link added

--- exNavigationLink2/ContentView.swift
+++ exNavigationLink2/ContentView.swift
@@ -4,13 +4,18 @@
 //
 //  Created by Benedito Macedo on 12/12/20.
 //
-
+// https://www.hackingwithswift.com/articles/216/complete-guide-to-navigationview-in-swiftui
+//
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            NavigationLink(destination: /*@START_MENU_TOKEN@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                Text("Hello World")
+            }
+            .navigationBarTitle("Navigation 3")
+        }
     }
 }
 
