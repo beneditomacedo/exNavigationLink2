e8ebb1b2ce53ef841c47d15a3f4f8b877d76d4c4
Benedito Alberto Macedo <benedito.macedo@gmail.com>
2020-12-12 23:26:28 -0300

    Initial Commit

---
+++ exNavigationLink2/exNavigationLink2App.swift
@@ -0,0 +1,17 @@
+//
+//  exNavigationLink2App.swift
+//  exNavigationLink2
+//
+//  Created by Benedito Macedo on 12/12/20.
+//
+
+import SwiftUI
+
+@main
+struct exNavigationLink2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
